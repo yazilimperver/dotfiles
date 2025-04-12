7bd470d2f7799938c1a33a708fb3e74128f337ee
Yazılımperver <34968082+yazilimperver@users.noreply.github.com>
2025-04-12 23:01:37 +0300

    betik guncellemeleri

--- .scripts/install_dots.sh
+++ .scripts/install_dots.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-git clone --bare https://github.com/yazilimperver/dotfiles.git $HOME/.cfg
-
 rm .bashrc
 rm .zshrc
 rm .vimrc
 rm -rf .cfg
 
+git clone --bare https://github.com/yazilimperver/dotfiles.git $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-config checkout
+config checkout -f
 config config status.showUntrackedFiles no
